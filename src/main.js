5e8e058a4a3b599382cb6b238bc69e72f80fa30b
Elioce33 <56270516+Elioce33@users.noreply.github.com>
2022-02-10 21:06:29 +0100

    update background color up to 4096

--- src/main.js
+++ src/main.js
@@ -17,11 +17,17 @@ for (let i = 0; i < 4; i++)
 //valuesTab[Math.floor(Math.random() * valuesTab.length)][Math.floor(Math.random() * valuesTab.length)] = new Cell(2);
 
 valuesTab[0][0] = new Cell(2);
-valuesTab[0][2] = new Cell(4);
-valuesTab[1][0] = new Cell(8);
-valuesTab[1][2] = new Cell(16);
-valuesTab[2][0] = new Cell(32);
-valuesTab[2][2] = new Cell(64);
+valuesTab[0][1] = new Cell(4);
+valuesTab[0][2] = new Cell(8);
+valuesTab[0][3] = new Cell(16);
+valuesTab[1][0] = new Cell(32);
+valuesTab[1][1] = new Cell(64);
+valuesTab[1][2] = new Cell(128);
+valuesTab[1][3] = new Cell(256);
+valuesTab[2][0] = new Cell(512);
+valuesTab[2][1] = new Cell(1024);
+valuesTab[2][2] = new Cell(2048);
+valuesTab[2][3] = new Cell(4096);
 
 const tab = new Table(valuesTab);
 tab_2048.innerHTML = tab.render();
