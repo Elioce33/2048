77c89c500426125d25c00730e9dae576e3db42a3
Elioce33 <56270516+Elioce33@users.noreply.github.com>
2022-02-15 12:39:23 +0100

    clean code

--- src/main.js
+++ src/main.js
@@ -4,16 +4,11 @@ import Cell from "./Cell.js";
 const tab_2048 = document.querySelector(".tab_2048");
 
 const valuesTab = [];
-valuesTab.push([]);
-valuesTab.push([]);
-valuesTab.push([]);
-valuesTab.push([]);
 
-for (let i = 0; i < 4; i++)
-    for (let j = 0; j < 4; j++)
-        valuesTab[i][j] = new Cell(0);
-
-// valuesTab[Math.floor(Math.random() * valuesTab.length)][Math.floor(Math.random() * valuesTab.length)] = new Cell(2);
+for (let i = 0; i < 4; i++){
+    valuesTab.push([]);
+    for (let j = 0; j < 4; j++) valuesTab[i][j] = new Cell(0);
+}
 
 const tab = new Table(valuesTab);
 tab.addRandomCell();
